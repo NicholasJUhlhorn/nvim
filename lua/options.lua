9f4f16aa366483957a80c344305ef849f2dc7452
NicholasJUhlhorn <nick.uhlhorn@gmail.com>
2024-04-16 16:38:38 -0700

    added header and indent option

--- lua/options.lua
+++ lua/options.lua
@@ -1,3 +1,6 @@
+-- Nicholas J Uhlhorn
+-- April 2024
+
 vim.opt.autoindent	= true			    -- indent new lines the same amount as last line.
 vim.opt.breakindent = true              -- indents wraped text to same as parent line.
 -- vim.opt.cc=88				        -- set color columns for good coding style.
@@ -8,6 +11,7 @@ vim.opt.ignorecase	= true			    -- case insensitve matching.
 vim.opt.number		= true			    -- add line numbers.
 --vim.opt.setmouse	='v'			    -- middle click paste.
 vim.opt.shiftwidth	= 4			        -- width for autoindents.
+vim.opt.showbreak   = string.rep(' ', 3)-- make it so long lines wrap smartly
 vim.opt.showmatch 	= true			    -- show matching brackets.
 vim.opt.smartcase   = true              -- case sensitive when using capitals in search
 vim.opt.softtabstop	= 4			        -- see multiple spaces as tabstops so <BS> does the right thing.
