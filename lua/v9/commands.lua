9c65c77d86146f1e0638e14a7456576e84358345
NicholasJUhlhorn <nick.uhlhorn@gmail.com>
2024-04-19 18:06:23 -0700

    added custom command Th to tab help with less characters

---
+++ lua/v9/commands.lua
@@ -0,0 +1,6 @@
+-- Nicholas J Uhlhorn
+-- April 2024
+
+vim.api.nvim_create_user_command('Th', 'tab help <args>', { nargs = 1})
+-- Was going to add :O for ':Oil .', but that seems to be the case already!
+
