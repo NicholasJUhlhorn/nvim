c08082a433999ff43d03f9d507d60a86461fbe5e
NicholasJUhlhorn <nick.uhlhorn@gmail.com>
2024-04-24 15:34:17 -0700

    added spellcheck shorthand command (for en_us)

--- lua/v9/commands.lua
+++ lua/v9/commands.lua
@@ -1,6 +1,9 @@
 -- Nicholas J Uhlhorn
 -- April 2024
 
-vim.api.nvim_create_user_command('Th', 'tab help <args>', { nargs = 1})
+vim.api.nvim_create_user_command('Th', 'tab help <args>', {nargs = 1})
 -- Was going to add :O for ':Oil .', but that seems to be the case already!
+
+-- Shorthand for enabling en_us spell check on a file.
+vim.api.nvim_create_user_command('Sc', 'setlocal spell spelllang=en_us', {nargs = 0})
 
