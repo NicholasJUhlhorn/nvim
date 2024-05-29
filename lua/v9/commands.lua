da29eace8d9f32c14dda570c4c8dbd8b0feb48d2
NickJUhlhorn <nick.uhlhorn@gmail.com>
2024-05-29 16:27:31 -0700

    autocommand that sets .hbs files to read as html files

--- lua/v9/commands.lua
+++ lua/v9/commands.lua
@@ -7,3 +7,8 @@ vim.api.nvim_create_user_command('Th', 'tab help <args>', {nargs = 1})
 -- Shorthand for enabling en_us spell check on a file.
 vim.api.nvim_create_user_command('Sc', 'setlocal spell spelllang=en_us', {nargs = 0})
 
+-- Add autocommand to set handlebar files as html
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile", "BufWinEnter"}, {
+    pattern = "*.hbs",
+    command = "set filetype=html",
+})
